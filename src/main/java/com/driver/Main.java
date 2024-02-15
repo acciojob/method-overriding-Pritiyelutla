3d74ac8a9d3f0dd36ce25e369db367b1b427c19e
Prit <pritiyelutla@gmail.com>
2024-02-15 23:16:47 +0530

    Task done 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,6 @@
 package com.driver;
 
-class A
-{
-    public String meth()
-    {
-        return "Invoking method from class A";
-    }
-}
-class B extends A
-{
-    public String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
-}
+
 public class Main {
     public static void main ( String args[])
     {
@@ -21,5 +8,19 @@ public class Main {
         String res = b.meth();
         System.out.println(res);
 
+    }
+    public static class A
+    {
+        public String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A
+    {
+        public String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
     }
 }
